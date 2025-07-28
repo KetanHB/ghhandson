14429f68e14fb2379d23f25890db626b9b09d104
KetanHB <ketan.bajoria@gmail.com>
2025-07-28 12:36:53 +0530

    This is my first commit

---
+++ src/test/java/ghhandson/tc/FirstTC.java
@@ -0,0 +1,11 @@
+package ghhandson.tc;
+
+import org.testng.annotations.Test;
+
+public class FirstTC {
+
+	@Test
+	public void addFirstTC() {
+		System.out.println("My first tc in FirstTC java file");
+	}
+}
